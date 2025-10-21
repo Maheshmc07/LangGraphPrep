872dbe0c790c508cae7cc8de7d05b9c234243622
Maheshmc07 <maheshmc2003@gmail.com>
2025-10-21 15:02:45 +0530

    Types of RAGs

--- BasicGraph/BasicProjects/mem0_agent.py
+++ BasicGraph/BasicProjects/mem0_agent.py
@@ -1,4 +1,21 @@
 from mem0 import Memory
 
-from langchain_google_genai import ChatGoogleGenerativeAI,
+from langchain_google_genai import ChatGoogleGenerativeAI,GoogleGenerativeAIEmbeddings
 import os
+config={
+    
+    "version":"v1.1",
+    "embedder":{
+        "provider"
+    }
+}
+
+
+
+
+vectore_store=Chroma(
+    embedding_function= GoogleGenerativeAIEmbeddings(model="models/embedding-001"),
+    persist_directory='pinecon_db',
+    collection_name='sample'
+    
+)
